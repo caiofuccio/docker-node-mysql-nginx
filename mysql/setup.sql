b700f0e2f5bb534a89d5d3314003bc2f9e018831
Caio Fuccio <caio.fuccio@gmail.com>
2024-07-18 23:11:51 -0300

    fix: containers dependencies and healthcheck

--- mysql/setup.sql
+++ mysql/setup.sql
@@ -1,3 +1,7 @@
-CREATE TABLE people (
-  nome VARCHAR(50)
+USE nodedb;
+
+CREATE TABLE IF NOT EXISTS `people`(
+    `id` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    PRIMARY KEY(`id`)
 );
--- node/index.js
+++ node/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 
 const app = express();
 const port = 3000;
@@ -7,20 +7,34 @@ const databaseConfig = {
     host: 'db',
     user: 'root',
     password: 'root',
-    database: 'db',
+    database: 'nodedb',
+    port: 3306,
 };
 
-const connection = mysql.createConnection(config);
+try {
+    const connection = await mysql.createConnection(databaseConfig);
 
-const nameToInsert = 'Caio';
-const query = `INSERT INTO people(name) values(${nameToInsert})`;
-client.query(query);
-connection.end();
+    const insertNewName = async (nameToInsert) => {
+        const query = `INSERT INTO people(name) values(${nameToInsert})`;
+        await connection.query(query, (error) => {
+            if (error) throw new Error({ error });
+        });
+        await connection.end();
+    };
 
-app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>');
-});
+    app.get('/', async (_, res) => {
+        try {
+            const nameToInsert = 'Caio';
+            await insertNewName(nameToInsert);
+            res.send('<h1>Full Cycle Rocks!</h1>');
+        } catch (error) {
+            throw new Error({ error });
+        }
+    });
 
-app.listen(port, () => {
-    console.log('Server is up on port ', port);
-});
+    app.listen(port, () => {
+        console.log('Server is up on port ', port);
+    });
+} catch (error) {
+    throw new Error({ error });
+}
